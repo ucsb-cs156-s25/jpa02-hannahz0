9608afd8f7f4fe06a44fc0f687e29f1dc9e4c7db
hannahz0 <85980913+hannahz0@users.noreply.github.com>
2025-04-08 13:26:29 -0700

    xy - updated with actual developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,10 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
         // You may use just the name that is used on <https://bit.ly/cs156-s25-teams>
         // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Hannah Z";
     }
 
     /**
@@ -30,7 +29,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "hannahz0";
     }
 
     /**
@@ -40,13 +39,13 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("s25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s25_07");
+        team.addMember("Annika D");
+        team.addMember("Hannya Y");
+        team.addMember("Shelly Z");
+        team.addMember("Forrest Z");
+        team.addMember("Steven L");
+        team.addMember("Hannah Z");
         return team;
     }
 }
